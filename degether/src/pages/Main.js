788e82f918cd11bd365a4b781f4bfa2c4e860536
sol <osunpine@gmail.com>
2022-06-30 17:04:38 +0900

    feat : Card component 완료

--- degether/src/pages/Main.js
+++ degether/src/pages/Main.js
@@ -5,21 +5,18 @@ function Main() {
   return (
     <>
       <CardGrid>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {Array.from({ length: 16 }, (item, idx) => {
+          return <Card key={idx} />;
+        })}
       </CardGrid>
     </>
   );
 }
 const CardGrid = styled.div`
+  margin-top: 215px;
   display: grid;
   grid-template-columns: repeat(8, 218px);
-  gap: 16px;
+  grid-column-gap: 16px;
+  grid-row-gap: 16px;
 `;
 export default Main;
