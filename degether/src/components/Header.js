a2366e65900194783bf09939a37d9b802b8622a8
sol <osunpine@gmail.com>
2022-06-30 17:54:06 +0900

    feat : SearchBar, Icon component 추가

--- degether/src/components/Header.js
+++ degether/src/components/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import HomeIcon, { NoticeIcon, Profile } from "./icon/Icon";
+
+import SearchBar from "./icon/SearchBar";
 
 const Header = () => {
   return (
     <>
       <HeaderContainer></HeaderContainer>
-      <MenuBar></MenuBar>
+      <MenuBar>
+        <SearchBar />
+        <IconContainer>
+          <HomeIcon />
+          <NoticeIcon />
+          <Profile />
+        </IconContainer>
+      </MenuBar>
     </>
   );
 };
@@ -24,6 +34,19 @@ const MenuBar = styled.div`
   left: 0px;
   top: 62px;
   background: #09120e;
+  display: flex;
+  align-items: center;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+  }
+`;
+const IconContainer = styled.div`
+  position: absolute;
+  right: 17px;
+  top: 40px;
 `;
 
 export default Header;
