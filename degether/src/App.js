42fe49eef9b76d69d1ddc97aefe078bbc57df1ae
JongHoonHong <hgh275@gmail.com>
2022-06-30 18:36:28 +0900

    라우팅 설정

--- degether/src/App.js
+++ degether/src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import MyPage from "./pages/MyPage";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
