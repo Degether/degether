0babe36f074605f089129a24d51b9b7d1cb3fc9e
sol <osunpine@gmail.com>
2022-07-04 12:48:11 +0900

    feat : 메인페이지 썸네일 카드 사이즈 변경

--- degether/src/components/main/Card.js
+++ degether/src/components/main/Card.js
@@ -5,12 +5,6 @@ const Card = () => {
   return (
     <>
       <CardContainer>
-        <CardText>
-          프로젝트 스케줄러 쿱(COUP)
-          <br />
-          <HeadCount>모집인원</HeadCount>{" "}
-          <HeadcountValue>[개발자 / 2명]</HeadcountValue>
-        </CardText>
         <CardImg>
           <Dday>
             <span>D</span>-10
@@ -34,13 +28,64 @@ const Card = () => {
             <PinCount>30</PinCount>
           </Pin>
         </CardImg>
+        <CardText>
+          프로젝트 스케줄러 쿱(COUP)
+          <br />
+          <HeadCount>모집인원</HeadCount>{" "}
+          <HeadcountValue>[개발자 / 2명] [디자이너 / 2명]</HeadcountValue>
+        </CardText>
       </CardContainer>
     </>
   );
 };
 
+const Card2 = () => {
+  return (
+    <>
+      <CardContainer2>
+        <CardImg2>
+          <Dday>
+            <span>D</span>-10
+          </Dday>
+          <img src="img/Rectangle 22.png" alt="프로젝트썸네일" />
+          <Pin>
+            <svg
+              width="7"
+              height="10"
+              viewBox="0 0 7 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M7 5.435C7 5.2 6.83 5.01 6.6 4.945C5.965 4.77 5.5 4.19 5.5 3.5V1H6C6.275 1 6.5 0.775 6.5 0.5C6.5 0.225 6.275 0 6 0H1C0.725 0 0.5 0.225 0.5 0.5C0.5 0.775 0.725 1 1 1H1.5V3.5C1.5 4.19 1.035 4.77 0.4 4.945C0.17 5.01 0 5.2 0 5.435V5.5C0 5.775 0.225 6 0.5 6H2.99L3 9.5C3 9.775 3.225 10 3.5 10C3.775 10 4 9.775 4 9.5L3.99 6H6.5C6.775 6 7 5.775 7 5.5V5.435Z"
+                fill="#EB3223"
+              />
+            </svg>
+            <PinCount>30</PinCount>
+          </Pin>
+        </CardImg2>
+        <CardText>
+          프로젝트 스케줄러 쿱(COUP)
+          <br />
+          <HeadCount>모집인원</HeadCount>{" "}
+          <HeadcountValue>[개발자 / 2명] [디자이너 / 2명]</HeadcountValue>
+        </CardText>
+      </CardContainer2>
+    </>
+  );
+};
 const CardContainer = styled.div`
   width: 218px;
+  height: 284px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+const CardContainer2 = styled.div`
+  width: 218px;
+  height: 466px;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
@@ -60,7 +105,20 @@ const HeadcountValue = styled.span`
 `;
 const CardImg = styled.div`
   width: 218px;
+  height: 218px;
   position: relative;
+  border-radius: 10px;
+  img {
+    width: 218px;
+    object-fit: cover;
+  }
+`;
+const CardImg2 = styled.div`
+  width: 218px;
+  height: 400px;
+  position: relative;
+  border-radius: 10px;
+  background: #ddd;
   img {
     width: 218px;
     object-fit: cover;
@@ -104,3 +162,4 @@ const PinCount = styled.div`
   font-weight: 700;
 `;
 export default Card;
+export { Card2 };
